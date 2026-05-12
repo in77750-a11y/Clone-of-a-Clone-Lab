ca03a07f5fd63c3797906d07b6445ec965a4a93f
in77750-a11y <in77750@mail.harpercollege.edu>
2026-05-11 22:40:04 -0500

    main file

--- main 2.cpp
+++ main 2.cpp
@@ -6,105 +6,133 @@
 //
 
 #include <iostream>
+#include <ctime>
+#include <cmath>
 
 using namespace std;
 
-//template <class T>
-class Person {
+double distortValue(double original, double maxPercent) {
+    double variation = original * maxPercent * ((rand() % 100 - 50) / 100.0);
+    return original + variation;
+}
+
+
+
+class MyPerson {
 private:
     string name;
-    int* age;
-    int* height;
-    int* copy_depth;
-    
-//    int age;
-//    int height;
-//    int copy_depth;
-    
-    
+    int age;
+    int height;
+    int copy_depth;
+    int clone_num;
 public:
-    Person(string n, int _age, int _height){
+    
+    
+    MyPerson(string n, int _age, int _height){
         name = n;
-        age = new int;
+        age = _age;
+        height = _height;
+        copy_depth = 0;
+        clone_num = 0;
         
+    }
+    
+    void distortion(){
         
-        height = new int;
+    }
+    
+    // Copy Constructor
+    MyPerson(const MyPerson &other) {
         
-        copy_depth = new int;
-        
-        //*name = n;
-        *age = _age;
-        *height = _height;
-        *copy_depth = 0;
+        copy_depth = other.copy_depth;
+        copy_depth++;
+        double defectProbability = 0.10 * copy_depth;
+                
+                // Generate random number between 0.0 and 1.0
+                double randomValue = (double)rand() / RAND_MAX;
 
+                if (randomValue < defectProbability) {
+                    // Mutation occurred!
+                    name = "Broken_" + other.name;
+                    age = other.age + (rand() % 10 - 5); // Age shifts slightly
+                    height = other.height * 0.9;         // Height shrinks
+                    cout << "[MUTATION] Clone " << copy_depth << " failed." << endl;
+                }
+                else {
+        
+            name = other.name;
+            age = other.age;
+            height = other.height;
+            
+            clone_num++;
+                        }
+        
+        
+        
+        
     }
     
-    Person(){
+    void print_COPY(){
+        clone_num = copy_depth;
+        
 
+        
+        if(copy_depth > 0){
+            cout << "Hello, My name is " << name + "_Clone_" << clone_num << ". I am " << age << " years old and my height is " << height << "." << endl;
+            
+            cout << "copy depth: " << copy_depth << endl;
+        }
+        
+        else{
+            cout << "Hello, My name is " << name << ". I am " << age << " years old and my height is " << height << "." << endl;
+                    
+                    cout << "copy depth: " << copy_depth << endl;
+        }
+        
+        
     }
-    
-    Person(const Person& other){
-        
-        age = new int;
-        
-        
-        height = new int;
-        
-        copy_depth = new int;
-
-
-        
-        
-        *age = *other.age;
-        *height = *other.height;
-        *copy_depth = *other.copy_depth;
-        copy_depth ++;
-    }
-    
-    ~Person(){
-        delete age, height, copy_depth;
-        
-        cout << "Deallocated" << endl;
-    }
-    
-    void operator=(const Person& other){
-        delete age, height, copy_depth;
-        
-        age = new int;
-        
-        
-        height = new int;
-        
-        copy_depth = new int;
-        
-        
-        *age = *other.age;
-        *height = *other.height;
-        *copy_depth = *other.copy_depth;
-    }
-    
-
-    
-    void print(){
-        cout << "Hello, My name is " << name << ". I am " << age << " years old and my height is " << height << "." << endl;
-        
-        cout << "copy depth: " << copy_depth << endl;
-    }
-    
-//    ~Person(){
-//        delete copy_depth;
-//    }
 };
+
+
+
 
 int main() {
     
-    Person p1 = Person("Hailey", 14, 5);
+    srand(time(nullptr));
     
-    Person p2 = p1;
     
-    p1.print();
+    MyPerson p1 = MyPerson("Hailey", 14, 5);
+    MyPerson p2 = p1;
+    MyPerson p3 = p2;
+    MyPerson p4 = p3;
+    MyPerson p5 = p4;
+    MyPerson p6 = p5;
+    
+    p1.print_COPY();
     cout << endl;
-    p2.print();
+    p2.print_COPY();
+    cout << endl;
+    p3.print_COPY();
+    cout << endl;
+    
+    p4.print_COPY();
+    cout << endl;
+    
+    p5.print_COPY();
+    cout << endl;
+    
+    p6.print_COPY();
+    cout << endl;
+    
+    
+    
+//    Person p1 = Person("Hailey", 14, 5);
+//    
+//    Person p2 = p1;
+//    
+//    p1.print();
+//    cout << endl;
+//    p2.print();
     
     
     
